59ed6cfc3e4e06fe93ed8ec6b89f69d04732c0fa
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-11-11 13:25:55 +0100

    Update configloader.js
    Fixed some typos

--- Script/configloader.js
+++ Script/configloader.js
@@ -2,8 +2,8 @@ const pathToConfig = './../Config/config.json'
 
 /**
  * This method can read the content of a JSON file. 
- * @param {*} file Path to the JSON file
- * @returns The contents of a JSON file
+ * @param {*} file - Path to the JSON file
+ * @returns - The contents of a JSON file
  */
 async function loadJSON() {
     try {
@@ -20,8 +20,8 @@ async function loadJSON() {
 
 /**
  * This method uses the key to find the matching value in a JSON file. 
- * @param {*} key Represents the key of a JSON file
- * @returns Returns the value of the key.
+ * @param {*} key - Represents the key of a JSON file
+ * @returns - Returns the value of the key.
  */
 async function getValueFromKey(key) {
     try {
